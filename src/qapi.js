be8a5bd6945e3e52355802ccb661c19cf70c2bcd
sergej <44100482+mrsv-dev@users.noreply.github.com>
2023-04-20 16:56:22 +0300

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"64414419aed6310020bfd76a",
+				"64414419aed6310020bfd76d"
+			]
+		},
+		"64414419aed6310020bfd76a": {
+			"id": "64414419aed6310020bfd76a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"64414419aed6310020bfd76d": {
+			"id": "64414419aed6310020bfd76d",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
